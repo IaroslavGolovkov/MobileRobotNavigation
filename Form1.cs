cab68a3e3cd25f9b0bf8bbb9a323266f902503f1
Iaroslav <golovkoviaroslav@gmail.com>
2021-11-14 13:34:00 +0400

    Реализовано движение и визуализация лидара

---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MobileRobotNavigation
+{
+    public partial class Form1 : Form
+    {        
+        Render field;
+        Robot robot;
+        Obstacles fieldObstacles;
+        Lidar lidar;
+        Plot plot;
+
+        bool ShowLidar = true;
+        bool ShowPlot = true;
+        bool DestinationSet = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fieldObstacles = new Obstacles(pictureBox1);
+            field = new Render(pictureBox1);
+            robot = new Robot(pictureBox1);
+            lidar = new Lidar(robot);
+            plot = new Plot(pictureBox2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            this.KeyPreview = true;
+            int num = int.Parse(textBox1.Text);
+            fieldObstacles.CreateObstacles(num);
+
+            field.RenderObstacles(fieldObstacles);
+            field.ShowObstacles(pictureBox1);
+            robot.Set_Default_Position();
+
+            lidar.UpdatePosition(robot);
+            lidar.FindDistance(field.obstaclesBmp);
+            
+            field.Clear();            
+            if (ShowLidar)
+                field.Refresh(pictureBox1, robot, lidar);
+            else
+                field.Refresh(pictureBox1, robot);
+            if (DestinationSet)
+                field.RenderDestinationPoint();
+            if (ShowPlot)
+                plot.SetData(lidar.Data);
+            else
+                plot.Clear();
+            plot.Show(pictureBox2);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    robot.moveForward = !robot.moveForward;
+                    if (robot.moveBackward)
+                        robot.moveBackward = false;
+                    //robot.Move(true);
+                    break;
+                case Keys.A:
+                    robot.moveLeft = !robot.moveLeft;
+                    if (robot.moveRight)
+                        robot.moveRight = false;
+                    //robot.Rotate(true);
+                    break;
+                case Keys.S:
+                    robot.moveBackward = !robot.moveBackward;
+                    if (robot.moveForward)
+                        robot.moveForward = false;
+                    //robot.Move(false);
+                    break;
+                case Keys.D:
+                    robot.moveRight = !robot.moveRight;
+                    if (robot.moveLeft)
+                        robot.moveLeft = false;
+                    //robot.Rotate(false);
+                    break;
+                //case Keys.Escape:
+                //    Application.Exit();
+                //    break;
+                default:
+                    break;
+            }            
+        }
+
+        private void PlotButton_Click(object sender, EventArgs e)
+        {
+            ShowPlot = !ShowPlot;
+            if (ShowPlot)
+                PlotButton.Text = "Скрыть график";
+            else
+                PlotButton.Text = "Показать график";
+        }
+
+        private void LidarButton_Click(object sender, EventArgs e)
+        {
+            ShowLidar = !ShowLidar;
+            if (ShowLidar)
+                LidarButton.Text = "Скрыть лидар";
+            else
+                LidarButton.Text = "Показать лидар";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            DestinationSet = true;
+            var mouseEventArgs = e as MouseEventArgs;
+            field.SetDestinationPoint((float)mouseEventArgs.X, (float)mouseEventArgs.Y);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(robot.moveForward)
+                robot.Move(true);           
+            if(robot.moveLeft)
+                robot.Rotate(true);
+            if(robot.moveBackward)
+                robot.Move(false);
+            if(robot.moveRight)
+                robot.Rotate(false);
+
+            if (robot.CollisionFound(fieldObstacles))
+            {
+                robot.Set_Default_Position();
+                //robot.moveForward = false;
+                //robot.moveBackward = false;
+                //robot.moveLeft = false;
+                //robot.moveRight = false;
+            }
+
+            lidar.UpdatePosition(robot);
+            lidar.FindDistance(field.obstaclesBmp);
+            field.Clear();
+            if (ShowLidar)
+                field.Refresh(pictureBox1, robot, lidar);
+            else
+                field.Refresh(pictureBox1, robot);
+            if (DestinationSet)
+                field.RenderDestinationPoint();
+            if (ShowPlot)
+                plot.SetData(lidar.Data);
+            else
+                plot.Clear();
+            plot.Show(pictureBox2);
+        }
+    }
+}
---
+++ Render.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MobileRobotNavigation
+{
+    class Render
+    {
+        public Bitmap bmp;  //Буфер для рисования
+        public Bitmap obstaclesBmp;
+        private Graphics obstaclesG;    //Изображение с препятствиями
+        private Graphics g; //Экземпляр Graphics
+        public int Width;  //Ширина поля
+        public int Height; //Высота поля
+
+        public float DestinationX;
+        public float DestinationY;
+        private const int DestinationPointSize = 21;
+        //private PictureBox pb;
+
+        public Render(PictureBox picturebox)    //Создание поверхности на основе PictureBox
+        {            
+            Width = picturebox.Width;
+            Height = picturebox.Height;
+            bmp = new Bitmap(Width, Height);
+            g = Graphics.FromImage(bmp);
+
+            obstaclesBmp = new Bitmap(Width, Height);
+            obstaclesG = Graphics.FromImage(obstaclesBmp);
+
+            DestinationX = 0;
+            DestinationY = 0;
+
+            Defaults();
+        }        
+        public void RenderObstacles(Obstacles obs)  //Отрисовка препятствий на отдельном graphics
+        {
+            obstaclesG.Clear(Color.White);
+            Pen pen = new Pen(Color.Black, 3.0F);
+            for (int i = 0; i < obs.numOfObstacles; i++)
+            {
+                obstaclesG.DrawEllipse(pen,
+                    obs.ObstacleX[i] - obs.ObstacleSize[i] / 2,
+                    obs.ObstacleY[i] - obs.ObstacleSize[i] / 2,
+                    obs.ObstacleSize[i],
+                    obs.ObstacleSize[i]);
+            }            
+        }
+        public void RenderDestinationPoint()
+        {
+            g.FillEllipse(Brushes.Yellow,
+                DestinationX - DestinationPointSize / 2,
+                DestinationY - DestinationPointSize / 2,
+                DestinationPointSize,
+                DestinationPointSize);
+        }
+        public void SetDestinationPoint(float x, float y)
+        {
+            DestinationX = x;
+            DestinationY = y;
+        }
+        public void ShowObstacles(PictureBox pb)
+        {
+            pb.BackgroundImage = obstaclesBmp;
+        }        
+        public void RenderRobot(Robot r)
+        {
+            g.FillEllipse(Brushes.White,
+                r.PositionX - r.Size / 2.0F,
+                r.PositionY - r.Size / 2.0F,
+                r.Size,
+                r.Size);
+            Pen pen = new Pen(Color.Black);
+            g.DrawEllipse(pen,
+                r.PositionX - r.Size / 2.0F,
+                r.PositionY - r.Size / 2.0F,
+                r.Size,
+                r.Size);
+            g.DrawLine(pen,
+                r.PositionX,
+                r.PositionY,
+                r.PositionX + r.Size / 2.0F * (float)Math.Cos(r.Angle),
+                r.PositionY + r.Size / 2.0F * (float)-Math.Sin(r.Angle));
+        }
+        public void RenderLidar(Lidar l)   //Отрисовка лучей лидара в буфере
+        {
+            for(int i = 0; i < l.Resolution; i++)
+            {
+                g.DrawLine(Pens.Red, l.PositionX, l.PositionY, l.DataX[i], l.DataY[i]);
+            }
+        }
+        public void Clear() //Очистка поверхности
+        {
+            g.Clear(Color.Transparent);
+        }
+        private void Show(PictureBox pb) //Отображение буфера на PictureBox
+        {            
+            pb.Image = bmp;
+        }
+        public void Refresh(PictureBox pb, Robot r, Lidar l)
+        {
+            //Clear();            
+            RenderRobot(r);
+            RenderLidar(l);
+            Show(pb);
+        }
+        public void Refresh(PictureBox pb, Robot r)
+        {
+            //Clear();
+            RenderRobot(r);
+            Show(pb);
+        }
+        private void Defaults() //Настройки по умолчанию
+        {
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            obstaclesG.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+    }
+}
