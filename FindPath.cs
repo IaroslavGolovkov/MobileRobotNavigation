7c439d5fbd65b5d92d84562950ecc0f256bcdf00
Iaroslav <golovkoviaroslav@gmail.com>
2021-11-14 19:12:43 +0400

    Добавлено создание градиента на поле

--- FindPath.cs
+++ FindPath.cs
@@ -20,9 +20,13 @@ namespace MobileRobotNavigation
         //Препятствия
         private Obstacles obs;
         //Распределение потенциалов по полю
-        List<List<double>> Potential;
+        double[,] Potential;
         //Точки маршрута
-        List<List<int>> Path;
+        
+        //Значения градиента для начальной и конечной точки, а также для препятствий
+        const double gradStart = 1;
+        const double gradDestination = -1;
+        const double gradObstacle = 1;
 
         public FindPath(Render rd, Robot r, Obstacles ob)
         {
@@ -32,11 +36,30 @@ namespace MobileRobotNavigation
             startY = (int)r.PositionY;
             destX = (int)rd.DestinationX;
             destY = (int)rd.DestinationY;
-            Potential = new List<List<double>>();
+            Potential = new double[width, height];
+            double A = destX - startX;
+            double B = destY - startY;
+            double C1 = A * startX + B * startY;
+            double C2 = A * destX + B * destY;
+            //Построение градиента
+            for(int x = 0; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    double C = A * x + B * y;
+                    
+                    Potential[x, y] = (gradStart * (C2 - C) + gradDestination * (C - C1))/(C2 - C1);
+                    for(int i = 0; i < ob.numOfObstacles; i++)
+                    {
+                        if (Math.Pow(x - ob.ObstacleX[x], 2) + Math.Pow(y - ob.ObstacleY[y], 2) < ob.ObstacleSize[i] + r.Size / 2)
+                            Potential[x, y] = gradObstacle;
+                    }
+                }
+            }
         }
-        public void moveToDestination(Robot r)
+        public void CalculatePath()
         {
-            r.MoveToPoint();
+
         }
     }
 }
